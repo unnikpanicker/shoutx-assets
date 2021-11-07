c72d0b5a6511989575412e75c5abf935e1d7fa4e
unnikpanicker <21nnikpanicker@gmail.com>
2021-11-07 09:09:34 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import UserProfileIcon from "./assets/icons/user-profile.png";
+import PasswordIcon from "./assets/icons/password.png";
+import FAQIcon from "./assets/icons/faq.png";
+import HeartIcon from "./assets/icons/heart.png";
+import LogoutIcon from "./assets/icons/logout.png";
+import NotificationIcon from "./assets/icons/notification.png";
+
+export { UserProfileIcon, PasswordIcon, FAQIcon,HeartIcon,LogoutIcon,NotificationIcon };
