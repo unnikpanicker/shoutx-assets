b790b043aea5bb5a70d1536b4c74db53715526dc
unnikpanicker <21nnikpanicker@gmail.com>
2021-11-08 08:10:10 +0530

    add: more icons

--- index.js
+++ index.js
@@ -4,5 +4,63 @@ import FAQIcon from "./assets/icons/faq.png";
 import HeartIcon from "./assets/icons/heart.png";
 import LogoutIcon from "./assets/icons/logout.png";
 import NotificationIcon from "./assets/icons/notification.png";
+import SearchLensIcon from "./assets/icons/search-lens.png";
+import ArrowRightIcon from "./assets/icons/arrow-right-24px.png";
+import ArrowRightDimIcon from "./assets/icons/arrow-right-24px-dim.png";
+import ClockIcon from "./assets/icons/clock.png";
+import MessageCopyIcon from "./assets/icons/message-copy.png";
+import MessageSendIcon from "./assets/icons/message-send.png";
+import MoreDescriptionIcon from "./assets/icons/more-description.png";
+import ShoutoutDetailsIcon from "./assets/icons/shoutout-details.png";
+import ShoutoutSpeakerIcon from "./assets/icons/shoutout-speaker.png";
+import UpIcon from "./assets/icons/up.png";
+import CalenderIcon from "./assets/icons/calender.png";
+import CaptionSmallIcon from "./assets/icons/caption-small.png";
+import CarouselScrollingArrowIcon from "./assets/icons/carousel-scrolling-arrow.png";
+import GradeIcon from "./assets/icons/grade-24px.png";
+import ImageSmallIcon from "./assets/icons/image-small.png";
+import ModalCloseIcon from "./assets/icons/modal-close.png";
+import MoreThreeDotIcon from "./assets/icons/more-three-dot.png";
+//menu icons
+import HomeIcon from "./assets/icons/home.png";
+import HomeSelectedIcon from "./assets/icons/home-selected.png";
+import InboxIcon from "./assets/icons/inbox.png";
+import InboxSelectedIcon from "./assets/icons/inbox-selected.png";
+import SavedIcon from "./assets/icons/saved.png";
+import SavedSelectedIcon from "./assets/icons/saved-selected.png";
+import SettingsIcon from "./assets/icons/settings.png";
+import SettingsSelectedIcon from "./assets/icons/settings-selected.png";
 
-export { UserProfileIcon, PasswordIcon, FAQIcon,HeartIcon,LogoutIcon,NotificationIcon };
+export {
+  UserProfileIcon,
+  PasswordIcon,
+  FAQIcon,
+  HeartIcon,
+  LogoutIcon,
+  NotificationIcon,
+  SearchLensIcon,
+  ArrowRightIcon,
+  ClockIcon,
+  MessageCopyIcon,
+  MessageSendIcon,
+  MoreDescriptionIcon,
+  ShoutoutDetailsIcon,
+  ShoutoutSpeakerIcon,
+  UpIcon,
+  ArrowRightDimIcon,
+  CalenderIcon,
+  CaptionSmallIcon,
+  CarouselScrollingArrowIcon,
+  GradeIcon,
+  HomeIcon,
+  HomeSelectedIcon,
+  ImageSmallIcon,
+  InboxIcon,
+  InboxSelectedIcon,
+  ModalCloseIcon,
+  MoreThreeDotIcon,
+  SavedIcon,
+  SavedSelectedIcon,
+  SettingsIcon,
+  SettingsSelectedIcon,
+};
